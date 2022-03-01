754875e28794da6fe643b19f8c5cc908739d9bc3
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-01 18:50:31 +0100

    non so perche non funziona il vinto/perso

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let str = prompt('txt')
+/* let str = prompt('txt')
 
 function reverseString(str) {
     
@@ -8,13 +8,56 @@ function reverseString(str) {
     
     let joinArray = reverseArray.join(""); 
     return joinArray; 
-}
+} */
 //console.log(reverseString(str));
 
-if (reverseString(str) == str) {
+/* if (reverseString(str) == str) {
     console.log ('palindromo')
 } else {
     console.log ('non palindromo')
-}
+} */
 //const myButton = document.getElementById('verificaPalindromo')
 //myButton.addEventListener ('click', reverseString())
+
+
+//utente sceglie pari o dispari
+// utente sceglie numero da 1 a 5
+// funzione numero random
+// somma =  numero utente  + numero random
+// if somma = scelta utente --> vinto
+// else perso
+
+const sceltaUtente = prompt('pari o dispari?')
+const numeroUtente = parseInt(prompt('scegli un numero da uno a cinque'))
+console.log(sceltaUtente)
+function getRandomNum() {
+    return Math.floor(Math.random() * 5);
+}
+
+
+
+let numeroRandom = getRandomNum() ;
+console.log(numeroUtente)
+
+console.log(numeroRandom)
+let somma = numeroRandom + numeroUtente ;
+console.log(somma)
+
+
+function oddEven (numeroRandom, numeroUtente) {
+     if (somma % 2 == 0) {
+         console.log('pari')
+     } else {
+         console.log('dispari')
+     }
+}
+
+const pariDispari = oddEven();
+
+
+
+if (pariDispari == sceltaUtente ) {
+    console.log('vinto')
+} else {
+    console.log('perso')
+}
